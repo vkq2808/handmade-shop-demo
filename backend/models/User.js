41d3b13c98918fbe1d06c460113eec1910bd41b2
Quoc <vkq0919309031@gmail.com>
2025-10-22 22:42:46 +0700

    feat: implement encryption for sensitive user data in User model

--- backend/models/User.js
+++ backend/models/User.js
@@ -1,4 +1,26 @@
 const mongoose = require('mongoose');
+require('dotenv').config();
+const crypto = require("crypto");
+const algorithm = "aes-256-gcm";
+const key = Buffer.from(process.env.ENCRYPTION_KEY, "hex");
+function encrypt(text) {
+  if (!text) return "";
+  const iv = crypto.randomBytes(12);
+  const cipher = crypto.createCipheriv(algorithm, key, iv);
+  let encrypted = cipher.update(text, "utf8", "hex");
+  encrypted += cipher.final("hex");
+  const authTag = cipher.getAuthTag().toString("hex");
+  return `${iv.toString("hex")}:${authTag}:${encrypted}`;
+}
+function decrypt(encryptedText) {
+  if (!encryptedText) return "";
+  const [ivHex, authTagHex, encryptedData] = encryptedText.split(":");
+  const decipher = crypto.createDecipheriv(algorithm, key, Buffer.from(ivHex, "hex"));
+  decipher.setAuthTag(Buffer.from(authTagHex, "hex"));
+  let decrypted = decipher.update(encryptedData, "hex", "utf8");
+  decrypted += decipher.final("utf8");
+  return decrypted;
+}
 
 const userSchema = new mongoose.Schema({
   name: {
@@ -74,5 +96,19 @@ const userSchema = new mongoose.Schema({
     }
   ]
 }, { timestamps: true });
+userSchema.pre("save", function (next) {
+  if (this.isModified("phone")) this.phone = encrypt(this.phone);
+  if (this.isModified("address")) this.address = encrypt(this.address);
+  next();
+});
+userSchema.path("phone").get(function (value) {
+  return decrypt(value);
+});
+
+userSchema.path("address").get(function (value) {
+  return decrypt(value);
+});
+userSchema.set("toObject", { getters: true });
+userSchema.set("toJSON", { getters: true });
 
 module.exports = mongoose.model('User', userSchema);
